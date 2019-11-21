302113d032d7bfdc206a3c4ff10c57ecc46c5d1a
B_emil <emil0729@mail.ru>
2019-11-22 00:07:02 +0100

    added simple menu (will be modificated)

--- Export/scripts/Main.js
+++ Export/scripts/Main.js
@@ -1,25 +1,25 @@
-
-$(document).ready(function () { Main(); });
+$(document).ready(function() { Main(); });
 var VC
 var board
 
 function Main() {
-	VC = new ViewControl();
-	//board = new GameBoard(24, 24, VC);
-	board = new EditorBoard(24, 24, VC);
-	var list = [{ Test: 'text 1' }, { Test: '2nd text' }, { Test: 'third example' }];
+    VC = new ViewControl();
+    //board = new GameBoard(24, 24, VC);
+    board = new EditorBoard(24, 24, VC);
+    var list = [{ Test: 'text 1' }, { Test: '2nd text' }, { Test: 'third example' }];
 
 
-	//==================================================================================================
-	//===================TESTING AND DEMONSTRATION======================================================
+    //==================================================================================================
+    //===================TESTING AND DEMONSTRATION======================================================
 
 
 
-	console.log(VC);
-	VC.DisplayList(list, "test_elem");
-	VC.ChangeView("editor");
-	$.getJSON("./exampleJsonFiles/gameState.json", function (json) {
-		console.log(json); // this will show the info it in firebug console
-		board.LoadState(json);
-	});
+    console.log(VC);
+    VC.DisplayList(list, "test_elem");
+    // VC.ChangeView("editor");
+    VC.ChangeView("main_menu");
+    $.getJSON("./exampleJsonFiles/gameState.json", function(json) {
+        console.log(json); // this will show the info it in firebug console
+        board.LoadState(json);
+    });
 }
